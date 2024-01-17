1aa5c41f8684fcb2e248036e594da902124ad35e
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2024-01-17 19:26:04 +0530

    Completed the code

--- SkillsBuild Masterclass 2.py
+++ SkillsBuild Masterclass 2.py
@@ -8,4 +8,34 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 
 #import dataset
-df=read_csv('')
+titanic=pd.read_csv(r'Path of titanic dataset')
+
+#importing sweetviz #pip install sweetviz
+import sweetviz as sv
+
+#analyzing the dataset 
+titanic_report = sv.analyze(titanic)
+
+#display the report
+titanic_report.show_html(titanic.html)
+
+#using dtale library
+d=dtale.show(titanic)
+d.open_browser()
+
+#import folium
+import folium
+
+# Coordinates for New York City
+latitude, longitude = 40.7128, -74.0060
+
+# Create a map centered at New York City
+m = folium.Map(location=[latitude, longitude], zoom_start=12)
+
+# Add a marker for New York City
+folium.Marker([latitude, longitude], popup='New York City').add_to(m)
+
+#save the map to an HTML file
+m.save('map.html')
+
+
